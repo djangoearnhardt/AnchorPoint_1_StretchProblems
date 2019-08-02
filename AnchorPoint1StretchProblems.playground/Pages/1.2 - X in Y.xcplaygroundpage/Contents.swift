840d4bb9bb0b574bd474b76b6c43a51513eb326d
Sambo <flatheadcatfish9@yahoo.com>
2019-08-02 12:43:12 -0600

    X in Y completed

--- AnchorPoint1StretchProblems.playground/Pages/1.2 - X in Y.xcplaygroundpage/Contents.swift
+++ AnchorPoint1StretchProblems.playground/Pages/1.2 - X in Y.xcplaygroundpage/Contents.swift
@@ -12,8 +12,16 @@ import Foundation
  ### Black Diamond 💎💎💎
  Make a method that prints every [Prime](https://en.wikipedia.org/wiki/Prime_number) number between **0** and **Y**.
  */
-
-
+func xInY(x: Int, y: Int) -> String {
+    for num in 0...y {
+        switch num {
+        case let z where num % x == 0: print("\(z)")
+        default: break
+        }
+    }
+    return "Finished"
+}
+xInY(x: 3, y: 15)
 
 
 
